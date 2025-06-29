2e15e0df91e1ab778ddf1459ab624c72a168ada0
gyanesh4u <37295102+gyanesh4u@users.noreply.github.com>
2025-06-30 00:07:31 +0530

    Demo 3

---
+++ src/test/Demo3.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		System.out.println(add(10, 20));
+	}
+	public static int add(int a,int b) {
+		return a+b;
+	}
+}
